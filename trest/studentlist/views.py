4b63c0104e7b7b2be1270d8d50b96b7c7fd3d42e
Leandro E. Colombo Viña <colomboleandro@bitson.com.ar>
2018-03-24 13:35:39 -0300

    Djanto Tutorial Part 3.2

--- trest/studentlist/views.py
+++ trest/studentlist/views.py
@@ -1,6 +1,4 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 from .models import Student
 
 # Create your views here.
@@ -8,8 +6,7 @@ from .models import Student
 
 def index(request):
     latest_students = Student.objects.order_by('-created_on')[:5]
-    template = loader.get_template('studentlist/index.html')
     context = {
         'latest_students': latest_students,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'studentlist/index.html', context)
