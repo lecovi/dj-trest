60b0bdb783a333a6118cb9c81c4fcf07bbb23431
Leandro E. Colombo Viña <colomboleandro@bitson.com.ar>
2018-03-24 13:55:21 -0300

    Djanto Tutorial Part 3.3

--- trest/studentlist/urls.py
+++ trest/studentlist/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:student_id>/', views.details, name='details'),
 ]
--- trest/studentlist/views.py
+++ trest/studentlist/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from .models import Student
 
@@ -10,3 +11,14 @@ def index(request):
         'latest_students': latest_students,
     }
     return render(request, 'studentlist/index.html', context)
+
+
+def details(request, student_id):
+    try:
+        student = Student.objects.get(pk=student_id)
+    except Student.DoesNotExist:
+        raise Http404('Student does not exist')
+    context = {
+        'student': student
+    }
+    return render(request, 'studentlist/details.html', context)
