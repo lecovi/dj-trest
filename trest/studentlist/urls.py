162e7030337e7053083f7a9ee1fafd50b1affffa
Leandro E. Colombo Viña <colomboleandro@bitson.com.ar>
2018-03-24 19:51:53 -0300

    Djanto Tutorial Part 3

--- trest/studentlist/urls.py
+++ trest/studentlist/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'studentlist'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:student_id>/', views.details, name='details'),
