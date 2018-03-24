061b467ccc7e1884f1e8299c629788cb24dd372d
Leandro E. Colombo Viña <colomboleandro@bitson.com.ar>
2018-03-24 13:20:02 -0300

    Djanto Tutorial Part 2

--- trest/studentlist/models.py
+++ trest/studentlist/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class DocType(models.Model):
+    doc_type = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.doc_type
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=200)
+    lastname = models.CharField(max_length=200)
+    doc_type = models.ForeignKey(DocType, on_delete=models.CASCADE)
+    doc_number = models.IntegerField()
+    email = models.EmailField()
+    created_on = models.DateTimeField('created on')
+
+    def __str__(self):
+        return '{}, {} <{}>'.format(self.lastname.upper(), self.name, self.email)
--- trest/trest/settings.py
+++ trest/trest/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'studentlist.apps.StudentlistConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'trest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'trest',
+        'USER': 'lecovi',
+        'PASSWORD': 'lecovi.trest',
+        'HOST': 'localhost',
     }
 }
 
