3b80c5daa93866287e54bc7a55bf25f379fbadf2
Leandro E. Colombo Viña <colomboleandro@bitson.com.ar>
2018-03-25 23:18:04 -0300

    Django REST quickstart

--- trest/trest/urls.py
+++ trest/trest/urls.py
@@ -15,9 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from quickstart import views
 from .views import index
 
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
 urlpatterns = [
+    path('api/rest/', include(router.urls)),
+    path('api/auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('', index),
     path('students/', include('studentlist.urls')),
     path('admin/', admin.site.urls),
